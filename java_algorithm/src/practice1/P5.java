8b2f88de6e7c5967527703bfb2f317fb2e86a25a
이신혜 <lop9036@naver.com>
2023-09-20 16:45:19 +0900

    첫 번째로 나오는 음수(배열)

--- java_algorithm/src/practice1/P5.java
+++ java_algorithm/src/practice1/P5.java
@@ -11,7 +11,7 @@ public class P5 {
     static class Solution {
     	 
     	 public int solution(int[] num_list) {
-    	        int answer = -1;
+    	        int answer = -1; //
     	        
     	        for(int i=0; i<num_list.length; i++) {
     	        	if(num_list[i] < 0) {
