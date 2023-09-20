386b368c0ce6444f0d27531681f61e5ff980fb0c
이신혜 <lop9036@naver.com>
2023-09-20 16:46:07 +0900

    첫 번째로 나오는 음수(배열)

---
+++ java_algorithm/src/practice1/P7.java
@@ -0,0 +1,30 @@
+package practice1;
+
+//첫 번째로 나오는 음수(배열) 
+public class P7 {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.solution(new int[] {12, 4, 15, 46, 38, -2, 15});
+    }
+
+    static class Solution {
+    	 
+    	 public int solution(int[] num_list) {
+    	        int answer = -1; //
+    	        
+    	        for(int i=0; i<num_list.length; i++) {
+    	        	if(num_list[i] < 0) {
+    	        		answer = i;
+    	        		System.out.println(answer);
+    	        		break;
+    	        	}
+    	        }
+    	        
+    	        return answer;
+    	    }
+ 
+    }
+}
+
+
